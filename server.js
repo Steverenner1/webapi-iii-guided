2c6f7f8027dc4403ff113decf706c1586cd1ea42
Steve Renner <che1111@hotmail.com>
2019-10-30 10:05:30 -0700

    Commit

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
+
 
 const server = express();
 
-server.use(express.json());
+
+function dateLogger(req, res, next) {
+  console.log(new Date().toISOString());
+
+  next();
+}
+
+function myLogger (req, res, next) {
+  console.log('GET api/hubs')
+  next()
+}
+
+// global middleware
+server.use(helmet());  // third party
+server.use(express.json());  // built in
+server.use(dateLogger); //custom middleware
+server.use(myLogger); // import custom middelware
 
 server.use('/api/hubs', hubsRouter);
 
